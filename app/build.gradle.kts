f2df9271571c755813adb07dac139f52a983c6cd
Fabien Boismoreau <fabien.boismoreau@gmail.com>
2023-09-20 00:06:57 +0200

    init the navigation host

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,10 +86,13 @@ android {
 dependencies {
 
     implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.navigation.runtime)
     implementation(libs.androidx.profile.installer)
 
-    implementation(libs.compose.material3)
+    implementation(libs.compose.animation)
     implementation(libs.compose.foundation)
+    implementation(libs.compose.material3)
     implementation(libs.compose.ui.tooling.preview)
     implementation(libs.compose.ui.ui)
 
