65b32c213e837cb2698f058bb486b298c4df0f04
Fabien Boismoreau <fabien.boismoreau@gmail.com>
2025-07-22 16:49:05 +0200

    Remove usage of firebase ktx module

--- common/analytics/analytics-impl/src/main/java/dev/bwaim/kustomalarm/analytics/impl/AnalyticsRepositoryImpl.kt
+++ common/analytics/analytics-impl/src/main/java/dev/bwaim/kustomalarm/analytics/impl/AnalyticsRepositoryImpl.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 Dev Bwaim team
+ * Copyright (c) 2025 Dev Bwaim team
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -7,48 +7,50 @@
  *
  *     https://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 
 package dev.bwaim.kustomalarm.analytics.impl
 
 import android.os.Bundle
+import com.google.firebase.Firebase
 import com.google.firebase.analytics.FirebaseAnalytics
-import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.analytics.analytics
 import com.google.firebase.analytics.logEvent
-import com.google.firebase.ktx.Firebase
 import dev.bwaim.kustomalarm.analytics.AnalyticsRepository
 import dev.bwaim.kustomalarm.analytics.model.KaEvent
 import javax.inject.Inject
 
-internal class AnalyticsRepositoryImpl @Inject constructor() : AnalyticsRepository {
-    private var firebaseAnalytics: FirebaseAnalytics = Firebase.analytics
+internal class AnalyticsRepositoryImpl
+    @Inject
+    constructor() : AnalyticsRepository {
+        private var firebaseAnalytics: FirebaseAnalytics = Firebase.analytics
 
-    override suspend fun logScreenView(
-        screenName: String,
-        screenClass: String,
-    ) {
-        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SCREEN_VIEW) {
-            param(FirebaseAnalytics.Param.SCREEN_NAME, screenName)
-            param(FirebaseAnalytics.Param.SCREEN_CLASS, screenClass)
+        override suspend fun logScreenView(
+            screenName: String,
+            screenClass: String,
+        ) {
+            firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SCREEN_VIEW) {
+                param(FirebaseAnalytics.Param.SCREEN_NAME, screenName)
+                param(FirebaseAnalytics.Param.SCREEN_CLASS, screenClass)
+            }
         }
-    }
 
-    override suspend fun logEvent(event: KaEvent) {
-        firebaseAnalytics.logEvent(event.name) {
-            event.params.forEach { (name, value) ->
-                when (value) {
-                    is Bundle -> param(name, value)
-                    is Double -> param(name, value)
-                    is Long -> param(name, value)
-                    is String -> param(name, value)
-                    else -> throw IllegalArgumentException("Wrong type for event parameter : ${value::class.java}")
+        override suspend fun logEvent(event: KaEvent) {
+            firebaseAnalytics.logEvent(event.name) {
+                event.params.forEach { (name, value) ->
+                    when (value) {
+                        is Bundle -> param(name, value)
+                        is Double -> param(name, value)
+                        is Long -> param(name, value)
+                        is String -> param(name, value)
+                        else -> throw IllegalArgumentException("Wrong type for event parameter : ${value::class.java}")
+                    }
                 }
             }
         }
     }
-}
