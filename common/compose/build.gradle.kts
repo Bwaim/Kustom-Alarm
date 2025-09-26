9f4f8a40f192c2f9b947a523de001a4b0f146aaf
Fabien Boismoreau <fabien.boismoreau@gmail.com>
2025-09-26 22:35:20 +0200

    Add missing dependency

--- common/compose/build.gradle.kts
+++ common/compose/build.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 Dev Bwaim team
+ * Copyright (c) 2025 Dev Bwaim team
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -7,11 +7,11 @@
  *
  *     https://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 plugins {
     id("kustomalarm.android.library")
@@ -37,6 +37,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.core)
 
+    implementation(libs.compose.material.icons.core)
     implementation(libs.compose.material3)
     implementation(libs.compose.runtime)
     implementation(libs.compose.ui.tooling.preview)
